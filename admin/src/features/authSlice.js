eeda075e75cae10f36ca57a6f21eae69bafd2249
MythiliKarthik <pppmythili@gmail.com>
2025-08-18 14:04:05 +0530

    some message

--- admin/src/features/authSlice.js
+++ admin/src/features/authSlice.js
@@ -1,7 +1,5 @@
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
-const API_BASE = process.env.NODE_ENV === "production"
-  ? "https://petshop-server.onrender.com"
-  : "http://localhost:5000";
+
 export const loginUser = createAsyncThunk(
     "auth/loginUser",
     async ({username, password}, {rejectWithValue}) => {
