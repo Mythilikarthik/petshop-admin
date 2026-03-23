a7705fee23712b34bcabded7605dca5802b8fbd0
MythiliKarthik <pppmythili@gmail.com>
2026-03-23 12:21:57 +0530

    some message

--- user/src/Pages/EditListings.js
+++ user/src/Pages/EditListings.js
@@ -328,14 +328,18 @@ const handleBannerChange = (e) => {
   }
 
   // Under review
-  if (listing?.status === "pending") {
+  const isPendingListing = listing?.status === "pending";
+const isPendingClaim =
+  listing?.isClaimed && listing?.claimStatus === "pending";
+
+if (isPendingListing || isPendingClaim) {
     return (
       
       <Container className="mt-5">
         <div className='pl-3 pr-3'>
         <Row className="mb-3 justify-content-end align-items-center">
           <Col>
-            <h2 className="main-title mb-0">Edit pending Listing</h2>
+            <h2 className="main-title mb-0">Edit Listing</h2>
             <Breadcrumb className="top-breadcrumb">
               <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
               <Breadcrumb.Item active>Edit Listing</Breadcrumb.Item>
@@ -353,34 +357,7 @@ const handleBannerChange = (e) => {
       </Container>
     );
   }
-  if(isClaimed) {
-    
-    if (listing?.claimStatus === "pending") {
-      return (
-        
-        <Container className="mt-5">
-          <div className='pl-3 pr-3'>
-          <Row className="mb-3 justify-content-end align-items-center">
-            <Col>
-              <h2 className="main-title mb-0">Edit claimed Listing</h2>
-              <Breadcrumb className="top-breadcrumb">
-                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                <Breadcrumb.Item active>Edit Listing</Breadcrumb.Item>
-              </Breadcrumb>
-            </Col>
-            
-          </Row>
-          <div className='form-container'>
-          <Alert variant="warning" className='text-center'>
-            <h5>Your listing is under review</h5>
-            <p>Please wait for admin approval.</p>
-          </Alert>
-          </div>
-          </div>
-        </Container>
-      );
-    }
-  }
+  
   return (
     <Container className="mt-4">
       <div className='pl-3 pr-3'>
